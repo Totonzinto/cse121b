0ccab1cdaa8b1657119ec762abc5e5c64a67598a
totonzinto <chiomanwosu67@gmail.com>
2024-01-20 16:33:42 +0100

    Added new feature

---
+++ scripts/w02-task.js
@@ -0,0 +1,28 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullname = 'Chioma Nwosu';
+const Year = new Date();
+let profilepicture = 'images/personal picture.jpg';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+imageElement.setAttribute('src',`${profilepicture}`);
+nameElement.innerHTML =(`<strong>${fullname}</strong>`);
+imageElement.setAttribute('alt', `profile image of ${fullname}`);
+
+
+/* Step 5 - Array */
+let foods = ['spaghetti', 'noodles', 'plantain', 'cereals'];
+foodElement.innerHTML +=`<br>${foods}`;
+foods.shift()
+foodElement.innerHTML +=`<br>${foods}`;
+foods.pop()
+foodElement.innerHTML +=`<br>${foods}`;
