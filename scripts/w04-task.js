5cd5e26c6dbdb89c2e467516b5820f717ca04741
totonzinto <chiomanwosu67@gmail.com>
2024-02-06 09:28:06 +0100

    made changes

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,66 +2,69 @@
 let photoElement = document.querySelector('#photo');
 /* Profile Object  */
 let myprofile = {
-    name: 'Chioma Nwosu',
-    photo: {
-      Src:  'images/personal picture.jpg',
-      alt: 'my profile picture'
-    },
+    name: "chioma Nwosu",
+    photo: "images/personal picture.jpg",
+    favoriteFoods: ['pizza', 'spaghetti', 'noodles', 'plaintain'],
+    hobbies: ['singing', 'music' ],
 
-    favoriteFood: ['pizza', 'spaghetti', 'noodles', 'plaintain'],
+    placesLived: [
+        {
+        place: 'port-harcourt, Nigeria',
+        length: '1 year',
+        },
+        {
+        place: 'Abia, Nigeria',
+        length: '19 years',
+        },
+        {
+        place: 'lagos Nigeria',
+        length: '4 years'
 
-    hobbies: ['singing', 'music', ],
-
-    placesLived: [],
+        }
+    ],
 };
 
 
 /* Populate Profile Object with placesLive objects */
-myprofile.placesLived.push(
-    {
-        place: 'port-harcourt, Nigeria',
-        length: '1 year',
-
-        place: 'Abia, Nigeria',
-        length: '19 years',
-
-        place: 'lagos Nigeria',
-        length: '4 years'
-
-    }
-)
-
-
 
 /* DOM Manipulation - Output */
 /* Name */
 document.querySelector("#name").innerHTML = myprofile.name;
 /* Photo with attributes */
-photoElement.Src = myprofile.photo.Src;
-photoElement.alt = myprofile.alt;
+photoElement.setAttribute("src", myprofile.photo)
+photoElement.setAttribute("alt", myprofile.photo)
 
-/* Favorite Foods List*/
-myprofile.favoriteFood.forEach(Food => {
-    let li = document.createElement('li');
-    li.textContent = Food;
-    document.querySelector('#favorite-foods').appendChild(li);
-});
+const foodsElement = document.querySelector("#favorite-foods");
+const placesElement = document.querySelector("#places-lived");
+const  hobbiesElement = document.querySelector("#hobbies");
 
-/* Hobbies List */
-myprofile.hobbies.forEach(hobby => {
-    let ul = document.createElement('li');
-    ul.textContent = hobby;
-    document.querySelector('#hobbies').appendChild(ul);
-});
+function generateListMarkup(list, templateCallback) {
+    const htmlList = list.map(templateCallback);
+    return htmlList.join("");
+}
 
-/* Places Lived DataList */
-myprofile.placesLived.forEach(function(place){
-    let dt = document.createElement('dd');
-    let dd = document.createElement('dt');
-    dt.textContent = place.place;
-    dd.textContent = place.length;
-    document.querySelector('#places-lived').appendChild(dt);
-    document.querySelector('#places-lived').appendChild(dd);
+function foodsTemplate(food) {
+    return `<li>${food}</li>`;
+  }
+  function hobbyTemplate(hobby) {
+    return `<li>${hobby}</li>`;
+  }
 
-});
+  function placesTemplate(place) {
+    return `<dt>${place.place}</dt><dd>${place.length}</dd>`;
+  }
 
+  foodsElement.innerHTML = generateListMarkup(
+    myprofile.favoriteFoods,
+    foodsTemplate
+  );
+  placesElement.innerHTML = generateListMarkup(
+    myprofile.placesLived,
+    placesTemplate
+  );
+
+  hobbiesElement.innerHTML = generateListMarkup (
+    myprofile.hobbies,
+    hobbyTemplate
+  );
+  
