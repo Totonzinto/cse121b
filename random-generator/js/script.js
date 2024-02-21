70aaa70c656c4086303d24ed0182789e58243e5a
totonzinto <chiomanwosu67@gmail.com>
2024-02-21 16:40:49 +0100

    made new content

---
+++ random-generator/js/script.js
@@ -0,0 +1,52 @@
+const date = new Date();
+
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
+document.getElementById('currentdate');
+currentdate.textContent = date.toLocaleDateString('en-GB', options);
+
+document.getElementById('theYear');
+theYear.textContent = date.getFullYear();
+
+
+
+const twitterButton = document.querySelector('#js-tweet');
+const spinner = document.querySelector('#js-spinner');
+const newQuoteButton = document.querySelector('#js-new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote() {
+  spinner.classList.remove('hidden');
+  newQuoteButton.disabled = true;
+
+  try {
+    const response = await fetch(endpoint)
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    displayQuote(json.message);
+		setTweetButton(json.message);
+  } catch {
+    alert('Failed to fetch new quote');
+  } finally {
+    newQuoteButton.disabled = false;
+    spinner.classList.add('hidden');
+  }
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+
+function setTweetButton(quote) {
+  twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote} - Donald Trump`);
+}
